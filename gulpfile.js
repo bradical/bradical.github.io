96c11aba1101f72f0f80670f67ebec475b15d8b2
Bradley Wagner <bradley.wagner@gmail.com>
2016-03-20 16:24:19 -0400

    Moved Sass, added normalize

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,36 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var rename = require('gulp-rename');
+var bower = require('gulp-bower');
+var sourcemaps = require('gulp-sourcemaps');
 
+var config = {
+  sassPath: './sass',
+  bowerDir: './bower_components'
+}
+
+gulp.task('bower', function() {
+  return bower()
+    .pipe(gulp.dest(config.bowerDir));
+});
 
 gulp.task('sass', function() {
-  return sass('styles.scss')
+  return sass(config.sassPath + '/*.scss', {
+      sourcemap: true,
+      loadPath: [
+        config.bowerDir + '/normalize-scss/sass',
+        config.bowerDir + '/support-for/sass',
+      ]
+    })
     .pipe(rename({suffix: '.min'}))
+    .on('error', sass.logError)
+
+    // For inline sourcemaps
+    .pipe(sourcemaps.write())
+
     .pipe(gulp.dest('.'));
 });
 gulp.task('watch', function (){
-  gulp.watch('*.scss', ['sass']);
+  gulp.watch([config.sassPath+'/*.scss'], ['sass']);
 });
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['bower','sass']);
